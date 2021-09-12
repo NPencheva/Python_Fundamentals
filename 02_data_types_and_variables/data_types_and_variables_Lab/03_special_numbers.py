9000ab43952ef7958c1c356af598af4c00a93d59
NPencheva <nvpencheva@gmail.com>
2021-09-12 14:08:18 +0300

    Update

--- 02_data_types_and_variables/data_types_and_variables_Lab/03_special_numbers.py
+++ 02_data_types_and_variables/data_types_and_variables_Lab/03_special_numbers.py
@@ -1,12 +1,31 @@
+# ВАРИАНТ 1:
+#
+# number = int(input())
+#
+# for n in range(1, number + 1):
+#     sum_of_digits = 0
+#     digits = n
+#
+#     while digits > 0:
+#         sum_of_digits += digits % 10
+#         digits = int(digits / 10)
+#     if sum_of_digits == 5 or sum_of_digits == 7 or sum_of_digits == 11:
+#         print(f"{n} -> True")
+#     else:
+#         print(f"{n} -> False")
+#
+# ------------------------------------------------------------
+# ВАРИАНТ 2:
+
 number = int(input())
 
 for n in range(1, number + 1):
     sum_of_digits = 0
-    digits = n
+    n_str = str(n)
 
-    while digits > 0:
-        sum_of_digits += digits % 10
-        digits = int(digits / 10)
+    for index in range(len(n_str)):
+        sum_of_digits += int(n_str[index])
+
     if sum_of_digits == 5 or sum_of_digits == 7 or sum_of_digits == 11:
         print(f"{n} -> True")
     else:
